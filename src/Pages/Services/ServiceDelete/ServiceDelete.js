278f378812cafb5e8994a40e22742fe391054c46
RiO <riohifi01@gmail.com>
2022-07-14 20:12:19 +0530

    v 0.1

---
+++ src/Pages/Services/ServiceDelete/ServiceDelete.js
@@ -0,0 +1,53 @@
+import React,{useEffect} from 'react';
+import  Button  from '@mui/material/Button';
+import Configs from '../../../Configs';
+
+
+const ServiceDelete = ({service}) => {
+     const token = localStorage.getItem('token')
+     let id = service._id
+          const handleClick = e =>{
+               var result = window.confirm("Want to delete?");
+               if (result==true) {
+                    // console.log('-->', id)
+                    fetch(`${Configs.baseUrl}admin/adminroute//${id}`, {
+                         method: 'DELETE',
+                         headers: {
+                         'content-type': 'application/json',
+                         'Authorization': token
+                         },
+                         body: JSON.stringify(),
+          
+                         })
+                         .then(res => res.json())
+                         .then(data => {
+                              console.log(data)
+                              if(data){
+                                   alert("Product deleted successfully")
+                                   window.location.reload();
+                              }
+                         })
+                         return true;
+               } else {
+               return false;
+               }
+               // e.preventDefault()
+          }
+
+   
+     
+     return (
+          <>
+               <Button
+                    color="error"
+                    variant="contained"
+                    size='small'
+                    onClick={handleClick}
+                    >
+                    Delete
+               </Button>
+          </>
+     );
+};
+
+export default ServiceDelete;
